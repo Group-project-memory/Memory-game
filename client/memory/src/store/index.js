db65dd67ccbc0350e6dcf1066e72f89ce78d3aac
budigk <budigk@gmail.com>
2021-07-09 00:12:07 +0700

    Add Random Card

--- client/memory/src/store/index.js
+++ client/memory/src/store/index.js
@@ -5,6 +5,68 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    cardList: [
+      {
+        num: 1,
+        id: "1b",
+        img: "https://playingcardcollector.files.wordpress.com/2013/03/d13204sa.jpg"
+      },
+      {
+          num: 2,
+          id: "2b",
+          img: "https://playingcardcollector.files.wordpress.com/2013/03/d13204s2.jpg"
+      },
+      {
+          num: 3,
+          id: "3b",
+          img: "https://playingcardcollector.files.wordpress.com/2013/03/d13204s3.jpg"
+      },
+      {
+          num: 4,
+          id: "4b",
+          img: "https://playingcardcollector.files.wordpress.com/2013/03/d13204s4.jpg"
+      },
+      {
+          num: 5,
+          id: "5b",
+          img: "https://playingcardcollector.files.wordpress.com/2013/03/d13204s5.jpg"
+      },
+      {
+          num: 6,
+          id: "6b",
+          img: "https://playingcardcollector.files.wordpress.com/2013/03/d13204s6.jpg"
+      },
+      {
+          num: 1,
+          id: "1r",
+          img: "https://playingcardcollector.files.wordpress.com/2013/03/d13204ha.jpg"
+      },
+      {
+          num: 2,
+          id: "2r",
+          img: "https://playingcardcollector.files.wordpress.com/2013/03/d13204h2.jpg"
+      },
+      {
+          num: 3,
+          id: "3r",
+          img: "https://playingcardcollector.files.wordpress.com/2013/03/d13204h3.jpg"
+      },
+      {
+          num: 4,
+          id: "4r",
+          img: "https://playingcardcollector.files.wordpress.com/2013/03/d13204h4.jpg"
+      },
+      {
+          num: 5,
+          id: "5r",
+          img: "https://playingcardcollector.files.wordpress.com/2013/03/d13204h5.jpg"
+      },
+      {
+          num: 6,
+          id: "6r",
+          img: "https://playingcardcollector.files.wordpress.com/2013/03/d13204h6.jpg"
+      },
+    ]
   },
   mutations: {
   },
