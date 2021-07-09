dc77a0c1f22d01909702194e1948e3eb137e5a41
budigk <budigk@gmail.com>
2021-07-09 15:32:39 +0700

    Dev-Home

--- client/memory/src/router/index.js
+++ client/memory/src/router/index.js
@@ -1,23 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import store from '../store/index.js'
-// import Home from '../views/Home.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
   {
     path: '/game',
     name: 'Game',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import('../views/Game.vue'),
+    component: () => import('../views/game.vue'),
   },
   {
     path: '/',
@@ -25,7 +25,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import('../views/Login.vue'),
+    component: () => import('../views/login.vue'),
     meta:{isLoginPage: true}
   }
 ]
